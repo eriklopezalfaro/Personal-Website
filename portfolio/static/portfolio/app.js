59f746a1c2d94a4f6e93f366470aff8594fdf201
Erik Lopez <eriklopezalfaro@outlook.com>
2021-10-13 00:04:47 -0700

    Created animation for landing page

---
+++ portfolio/static/portfolio/app.js
@@ -0,0 +1,9 @@
+// using gsap to animate the sliders
+
+const tl = gsap.timeline({defaults: {ease: "power1.out"}})
+
+tl.to('.text', {y: "0%", duration: 1, stagger: 0.35})
+tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.5})
+tl.to('.intro', {y : "-100%", duration: 1}, "-=1.2")
+tl.fromTo('.wrapper', {opacity: 0}, {opacity: 1, duration: 1})
+tl.fromTo('.hero-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
