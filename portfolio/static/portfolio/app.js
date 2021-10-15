ac3a4792941a466a0f713e4347c1d9a434152ea2
Erik Lopez <eriklopezalfaro@outlook.com>
2021-10-14 20:54:39 -0700

    contact page fix and nav bar transition changes made

--- portfolio/static/portfolio/app.js
+++ portfolio/static/portfolio/app.js
@@ -1,5 +1,4 @@
 // using gsap to animate the sliders
-
 const tl = gsap.timeline({defaults: {ease: "power1.out"}})
 
 tl.to('.text', {y: "0%", duration: 1, stagger: 0.35})
@@ -7,3 +6,20 @@ tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.5})
 tl.to('.intro', {y : "-100%", duration: 1}, "-=1.2")
 tl.fromTo('.wrapper', {opacity: 0}, {opacity: 1, duration: 1})
 tl.fromTo('.hero-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
+
+$(function(){
+    $(document).scroll(function(){
+        var $nav = $(".wrapper");
+        var $icons = $(".icons");
+        var $logos = $(".icon");
+        var $logo = $(".logo");
+        var $resume = $(".resume");
+        var $head = $(".hero-image");
+
+        $nav.toggleClass('scrolled', $(this).scrollTop() >= ($head.height()));
+        $logos.toggleClass('scrolled', $(this).scrollTop() >= ($head.height()));
+        $logo.toggleClass('scrolled', $(this).scrollTop() >= ($head.height()));
+        $icons.toggleClass('scrolled', $(this).scrollTop() >= ($head.height()));
+        $resume.toggleClass('scrolled', $(this).scrollTop() >= ($head.height()));
+    });
+});
