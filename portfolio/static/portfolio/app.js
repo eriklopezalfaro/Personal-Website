4fdf9a87a6d80d5a244b7588a235999116467c2c
Erik Lopez <eriklopezalfaro@outlook.com>
2021-11-19 22:38:04 -0800

    cleaned up css media queries

--- portfolio/static/portfolio/app.js
+++ portfolio/static/portfolio/app.js
@@ -29,3 +29,7 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+window.onscroll() = () => {
+    navbar.classList.remove('active');
+}
